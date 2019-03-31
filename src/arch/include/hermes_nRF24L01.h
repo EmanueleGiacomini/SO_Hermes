01ceabc87f3c76650da63ef166eb29f30048a38f
RickyMexx <riccardo.caprari@gmail.com>
2019-03-31 11:08:10 +0200

    Nrf24l01+Spi is working after 2 weeks of technical and spiritual problems

--- src/arch/include/hermes_nRF24L01.h
+++ src/arch/include/hermes_nRF24L01.h
@@ -1,4 +1,97 @@
-#include "NRF24L01.h"
+
+#ifndef _NRF24L01_H_
+#define _NRF24L01_H_
+
+#include <avr/io.h>
+
+//CE and CSN port definitions
+#define NRF24L01_DDR DDRB
+#define NRF24L01_PORT PORTB
+#define NRF24L01_CE PB5 //PB5 - PIN 11
+#define NRF24L01_CSN PB4 //PB4 - PIN 10
+
+//CE and CSN functions
+#define nrf24l01_CSNhi NRF24L01_PORT |= (1<<NRF24L01_CSN);
+#define nrf24l01_CSNlo NRF24L01_PORT &= ~(1<<NRF24L01_CSN);
+#define nrf24l01_CEhi NRF24L01_PORT |=  (1<<NRF24L01_CE);
+#define nrf24l01_CElo NRF24L01_PORT &= ~(1<<NRF24L01_CE);
+
+//power setup
+#define NRF24L01_RF24_PA_MIN 1
+#define NRF24L01_RF24_PA_LOW 2
+#define NRF24L01_RF24_PA_HIGH 3
+#define NRF24L01_RF24_PA_MAX 4
+#define NRF24L01_RF24_PA NRF24L01_RF24_PA_MAX
+
+//speed setup
+#define NRF24L01_RF24_SPEED_250KBPS 1
+#define NRF24L01_RF24_SPEED_1MBPS 2
+#define NRF24L01_RF24_SPEED_2MBPS 3
+#define NRF24L01_RF24_SPEED NRF24L01_RF24_SPEED_1MBPS
+
+//crc setup
+#define NRF24L01_RF24_CRC_DISABLED 1
+#define NRF24L01_RF24_CRC_8 2
+#define NRF24L01_RF24_CRC_16 3
+#define NRF24L01_RF24_CRC NRF24L01_RF24_CRC_16
+
+//transmission channel
+#define NRF24L01_CH 76
+
+//payload lenght
+#define NRF24L01_PAYLOAD 16
+
+//auto ack enabled
+#define NRF24L01_ACK 1
+
+//auto retransmit delay and count
+#define NRF24L01_RETR (0b0100 << NRF24L01_REG_ARD) | (0b0111 << NRF24L01_REG_ARC) //1500uS, 15 times
+
+//enable / disable pipe
+#define NRF24L01_ENABLEDP0 1 //pipe 0
+#define NRF24L01_ENABLEDP1 1 //pipe 1
+#define NRF24L01_ENABLEDP2 1 //pipe 2
+#define NRF24L01_ENABLEDP3 1 //pipe 3
+#define NRF24L01_ENABLEDP4 1 //pipe 4
+#define NRF24L01_ENABLEDP5 1 //pipe 5
+
+//address size
+#define NRF24L01_ADDRSIZE 5
+
+//pipe address
+#define NRF24L01_ADDRP0 {0xE8, 0xE8, 0xF0, 0xF0, 0xE2} //pipe 0, 5 byte address
+#define NRF24L01_ADDRP1 {0xC1, 0xC2, 0xC2, 0xC2, 0xC2} //pipe 1, 5 byte address
+#define NRF24L01_ADDRP2 {0xC1, 0xC2, 0xC2, 0xC2, 0xC3} //pipe 2, 5 byte address
+#define NRF24L01_ADDRP3 {0xC1, 0xC2, 0xC2, 0xC2, 0xC4} //pipe 3, 5 byte address
+#define NRF24L01_ADDRP4 {0xC1, 0xC2, 0xC2, 0xC2, 0xC5} //pipe 4, 5 byte address
+#define NRF24L01_ADDRP5 {0xC1, 0xC2, 0xC2, 0xC2, 0xC6} //pipe 5, 5 byte address
+#define NRF24L01_ADDRTX {0xE8, 0xE8, 0xF0, 0xF0, 0xE2} //tx default address*/
+
+ //enable print info function
+#define NRF24L01_PRINTENABLE 1
+
+// Basic functions
+extern void nrf24l01_init(void);
+extern uint8_t nrf24l01_getstatus(void);
+extern uint8_t nrf24l01_readready(uint8_t* pipe);
+extern void nrf24l01_read(uint8_t *data);
+extern uint8_t nrf24l01_write(uint8_t *data);
+extern void nrf24l01_setrxaddr(uint8_t channel, uint8_t *addr);
+extern void nrf24l01_settxaddr(uint8_t *addr);
+
+// Extra functions
+void nrf24l01_setpalevel(void);
+void nrf24l01_setdatarate(void);
+void nrf24l01_setcrclength(void);
+void nrf24l01_flushRXfifo(void);
+void nrf24l01_flushTXfifo(void);
+void nrf24l01_setRX(void);
+void nrf24l01_setTX(void);
 
 
+#if NRF24L01_PRINTENABLE == 1
+extern void nrf24l01_printinfo(void(*prints)(const char *), void(*printc)(unsigned char data));
+#endif
+
+#endif
 
--- src/arch/include/spi.h
+++ src/arch/include/spi.h
@@ -1,77 +1,12 @@
-/**
- * spi.h
- *
- * This section handle the low level mechanics of SPI module.
- * TODO: Add explanations of SPI module
- **/
+#include <avr/io.h>
 
-#pragma once
-#include <stdint.h>
+// SPI Pins
+#define SPI_DDR DDRB
+#define SPI_PORT PORTB
+#define SPI_MISO PB3 //PB3 - PIN 50
+#define SPI_MOSI PB2 //PB2 - PIN 51
+#define SPI_SCK PB1 //PB1 - PIN 52
+#define SPI_SS PB0 //PB0 - PIN 53
 
-#define BUF_SIZE 256
-
-
-enum SPI_MODE {
-  MASTER,
-  SLAVE
-};
-
-struct Spi;
-
-/**
- * Initialize the Spi module
- **/
-struct Spi* Spi_init(uint32_t);
-
-/**
- * Insert c in the spi buffer, ready to be sent
- **/
-void Spi_write(struct Spi*, uint8_t);
-
-/**
- * Return the number of available bytes to read from the spi
- **/
-uint8_t Spi_available(struct Spi*);
-
-/**
- * reads the first character from the spi buffer
- **/
-uint8_t Spi_read(struct Spi*);
-
-/**
- * Returns the number of free cells inside the tx buffer
- **/
-uint16_t Spi_txFree(struct Spi*);
-
-
-
-
-
-//------------------------ Low level functions ------------------------// 
-
-/**
- * Insert c in the spi buffer, ready to be sent
- **/
-void spi_write(uint8_t c);
-
-/**
- * Return the number of available bytes to read from the spi bus
- **/
-uint8_t spi_available();
-
-/**
- * reads the first character from the spi buffer
- **/
-uint8_t spi_read();
-
-/**
- * Initialize SPI master
- **/
-
-void spi_init_master();
-
-/**
- * Initialize SPI slave
- **/
-
-void spi_init_slave();
+extern void spi_init(void);
+extern uint8_t spi_writereadbyte(uint8_t data);
